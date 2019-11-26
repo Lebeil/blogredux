0257c7f4d53f14c932ccdf4aaa8b1e4e837aafae
Lebeil <liedel75@gmail.com>
2019-11-26 13:26:13 +0100

    deploy
    ajout +

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -6,8 +6,8 @@ import {signOut} from "../../store/actions/authActions";
 const SignedInLinks = ({signOut, profile}) => {
     return (
         <ul className="right">
-            <li><NavLink to='/create'>New Project</NavLink></li>
-            <li><a onClick={signOut}>Log Out</a></li>
+            <li><NavLink to='/create'>+</NavLink></li>
+            <li><a onClick={signOut}>Log out</a></li>
             <li><NavLink to='/' className='btn btn-floating pink lighten-1'>{profile.initials}</NavLink></li>
         </ul>
 
