0fc38a680cd11e9671b0ad1a0c383bb23c2debfe
Lebeil <liedel75@gmail.com>
2019-11-27 14:47:54 +0100

    final

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -3,17 +3,26 @@ import {deleteProject} from "../../store/actions/projectActions";
 import {connect} from 'react-redux'
 import moment from "moment";
 import 'moment/locale/fr';
+import {compose} from 'redux'
+import {firestoreConnect} from 'react-redux-firebase'
 
-const ProjectSummary = ({project, deleteProject}) => {
+const ProjectSummary = ({project, projects, deleteProject, auth}) => {
+
     const handleClick = (e)=> {
         e.preventDefault()
         deleteProject(project.id)
     }
 
+
+        if(auth.id === projects.id){
+            return
+        }
+
+
     return (
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
-                    <button className='btn-floating right btn-small red-text white' onClick={handleClick}>X</button>
+                    {auth.uid ? <button className='btn-floating right btn-small red-text white' onClick={handleClick}>X</button> : null }
                     <span className="card-title">{project.title}</span>
                         <p>Posté par {project.authorFirstName} {project.authorLastName}</p>
                         <p className='grey-text'>{moment(project.createdAt.toDate().toString()).calendar()}</p>
@@ -23,4 +32,17 @@ const ProjectSummary = ({project, deleteProject}) => {
     );
 };
 
-export default connect(null,{deleteProject})(ProjectSummary);
+const mapStateToProps = (state)=> {
+    return {
+        auth: state.firebase.auth,
+        projects: state.firestore.ordered.projects,
+    }
+}
+
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ]),
+    {deleteProject})(ProjectSummary);
